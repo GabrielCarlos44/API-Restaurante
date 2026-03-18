37b3d3384f294ff5bf69cac6fb1a63014d0dc525
Gabriel Carlos e Josue Xavier <gabrielcarlosnm@unifesspa.edu.br>
2026-03-18 19:33:22 -0300

    get, craete and put

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,17 +1,66 @@
 import { NextFunction, Request, Response } from "express"
-import { AppError } from "../../utils/AppError"
+import { knex } from "@/database/knex"
+import { z } from "zod"
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction){
     try {
+      const { name } =  request.query
+      const products = await knex<ProductRepository>("products")
+      .select()
+      .whereLike("name", `%${name ?? " "}%`)
+      .orderBy("name")
 
     
-      return response.json({message: "ok"})
+      return response.json(products)
 
     } catch (error){
       next(error)
     }
 
   }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+       const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, { message:"value must be greater than 0" }),
+       })
+
+       const { name, price } = bodySchema.parse(request.body)
+
+       await knex<ProductRepository>("products").insert({ name, price })
+
+       return response.status(201).json()
+ 
+    } catch(error){
+      next(error)
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+    const id =  z.string()
+    .transform((value) => Number(value))
+    .refine((value) => !isNaN(value),{ message:"id must be a number"})
+    .parse(request.params.id)
+
+    const bodySchema = z.object({
+      name: z.string().trim().min(6),
+      price: z.number().gt(0),
+    
+    })
+
+    const { name, price} =  bodySchema.parse(request.body)
+
+
+
+      return response.json({ message: "updated" })
+
+    } catch(error){
+      next(error)
+    }
+  }
+
 }
 
 export { ProductController }
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -4,5 +4,6 @@ const productsRoutes = Router()
 const productsController = new ProductController()
 
 productsRoutes.get("/", productsController.index)
-
+productsRoutes.post("/", productsController.create)
+productsRoutes.put("/:id", productsController.update)
 export { productsRoutes}

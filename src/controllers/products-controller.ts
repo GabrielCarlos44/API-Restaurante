643449655468bb96140495ab5a65d779d46b8071
Gabriel Carlos e Josue Xavier <gabrielcarlosnm@unifesspa.edu.br>
2026-03-19 15:33:55 -0300

    create, list, update and delete

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 import { knex } from "@/database/knex"
 import { z } from "zod"
+import { AppError } from "../../utils/AppError"
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction){
     try {
@@ -52,14 +53,49 @@ class ProductController {
 
     const { name, price} =  bodySchema.parse(request.body)
 
+    const product = await knex<ProductRepository>("products")
+      .select()
+      .where({ id })
+      .first()
 
+     if(!product){
+      throw new AppError("product not found")
 
-      return response.json({ message: "updated" })
+      await knex<ProductRepository>("products")
+      .update({ name, price, updated_at: knex.fn.now() })
+      .where({ id })
+      return response.json()}
 
     } catch(error){
       next(error)
     }
   }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id =  z
+      .string()
+      .transform((value) => Number(value))
+      .refine((value) => !isNaN(value),{ message:"id must be a number"})
+      .parse(request.params.id)
+
+      const product = await knex<ProductRepository>("products")
+      .select()
+      .where({ id })
+      .first()
+
+     if(!product){
+      throw new AppError("product not found")
+     } 
+
+      await knex<ProductRepository>("products").delete().where({ id })
+      return response.status(204).send()
+     } catch (error) {
+      next(error)
+     }
+  
+
+    }
 
 }
 
